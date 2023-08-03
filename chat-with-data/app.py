0a8bdcddf1e707aca67da28c2bb82c31974456a2
Kashif-Raza6 <96944736+Kashif-Raza6@users.noreply.github.com>
2023-08-03 15:14:16 +0500

    Update app.py

--- chat-with-data/app.py
+++ chat-with-data/app.py
@@ -42,7 +42,7 @@ def get_answer_csv(file: TextIO, query: str) -> str:
     #df = pd.read_csv("titanic.csv")
 
     # Create an agent using OpenAI and the Pandas dataframe
-    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=False)
+    agent = create_csv_agent(OpenAI(temperature=0, openai_api_key = OPENAI_API_KEY), file, verbose=False)
     #agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
 
     # Run the agent on the given query and return the answer
